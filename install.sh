281c491ba13bd6e4f6bc79622e24b8c105b990c7
ciazhar <ciazhar.id@gmail.com>
2018-05-10 00:51:19 +0700

    add bash file

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "|---------------------------------|"
+echo "|-------Tea Installer GTK---------|"
+echo "|---------------------------------|"
+
+echo ""
+echo ""
+
+echo "defining var for execute"
+USR=./tea-installer-gtk/usr
+USR_ROOT=/usr
+
+echo ""
+echo ""
+
+echo "deploying"
+cp $USR $USR_ROOT
+
+echo ""
+echo ""
+echo "done~"
+
+echo "|---------------------------------|"
+echo "|------INSTALLATION COMPLETE0-----|"
+echo "|---------------------------------|"
